a3eac3f94781ac76ecb1f9e24f8930da1ea9ba4c
Your Name <you@example.com>
2019-06-17 03:07:44 +0300

    diplom musher fu.....

---
+++ src/test/java/org/o7planning/sbshoppingcart/SbHibernateShoppingCartApplicationTests.java
@@ -0,0 +1,16 @@
+package org.o7planning.sbshoppingcart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbHibernateShoppingCartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
